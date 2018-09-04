a2db8d705e0dba76862e05a72d0389989d7eeb46
“Aisling <aisling1odonovan@gmail.comgit commit -m Initial>
2018-09-04 19:33:59 +0100

    3rd

--- settings/staging.py
+++ settings/staging.py
@@ -1,10 +1,11 @@
 from base import *
+import dj_database_url
 
 DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'default': dj_database_url.config('CLEARDB_DATABASE_URL'),
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
